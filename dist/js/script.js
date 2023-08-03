8903b4165db7364c2141fa7d13feb804625462e3
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2023-08-03 10:37:12 +0800

    wow

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,6 @@ const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
 
 toggleMenu.addEventListener('click', () => {
-    headerNav.classList.toggle('!h-[24rem]');
+    headerNav.classList.toggle('!h-[20.5rem]');
 });
 
